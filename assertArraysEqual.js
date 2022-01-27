0346393fff5c9a39a910310352b6ba6b21841e9b
hseiji <henrique.hst@gmail.com>
2022-01-27 18:45:18 -0500

    implemented assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+const eqArrays = (actual, expected) => {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+  return true;
+  }
+};
+
+
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual("Lighthouse Labs", "Bootcamp");
+assertArraysEqual(1, 1);
+assertArraysEqual("aa", "Aa");
+assertArraysEqual("1", 1);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
