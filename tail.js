06d6f85860de8f526e7a1fac21963f5fedd8501a
hseiji <henrique.hst@gmail.com>
2022-01-26 19:28:45 -0500

    commit tail

---
+++ tail.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+// const wordsB = [];
+console.log(tail(words));
+assertEqual(words.length, 3);
