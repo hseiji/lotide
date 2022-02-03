c694a5c7688bd0d75f55b7499a691ce97d9a104b
hseiji <henrique.hst@gmail.com>
2022-02-02 19:46:36 -0500

    findKey.js completed

--- takeUntil.js
+++ takeUntil.js
@@ -30,12 +30,12 @@ const takeUntil = function(array, callback) {
   //   } else {
   //     result.push(element)
   //   }
-  // });
-  for (let i = 0; i < array.length; i++) {
-    if (callback(array[i])) {
+  // });       ---- Be aware forEach does not stop! ----
+  for (const elem of array) {
+    if (callback(elem)) {
       return result;
     } else {
-      result.push(array[i]);
+      result.push(elem);
     }
   }
 
@@ -46,9 +46,11 @@ const takeUntil = function(array, callback) {
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
