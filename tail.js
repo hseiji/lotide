2f64050b8013840ebe0514534d2726f86341d612
hseiji <henrique.hst@gmail.com>
2022-02-13 10:24:51 -0500

    refactored tail.js

--- tail.js
+++ tail.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(array) {
   return array.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-// const wordsB = [];
-console.log(tail(words));
-assertEqual(words.length, 3);
+module.exports = tail;
