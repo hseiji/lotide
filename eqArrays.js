6c16f4fc0e5b94dd226a86b35bdf5fc1c48cae7b
hseiji <henrique.hst@gmail.com>
2022-02-13 10:02:34 -0500

    created test folder for assertEqual and head

--- eqArrays.js
+++ eqArrays.js
@@ -32,11 +32,11 @@ const eqArrays = (actual, expected) => {
 
 
 // TESTS --------------------------------------------------------------
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+// console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
 console.log(eqArrays([[1, [2, [4]]], 3], [[1, [2, [4]]], 3])); // => true
 
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
